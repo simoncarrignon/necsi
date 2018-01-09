3b5731ee7d2d5d9d4f9dd52b883ce95ff70c9933
Simon aka @simille <simon@elisya.org>
2018-01-09 04:48:25 +0100

    small changes in comments

--- clusterTrade.py
+++ clusterTrade.py
@@ -5,23 +5,22 @@ import matplotlib.pyplot as pl
 
 ##first we get the import and the export in two dataframe using panda 
 all_import=pd.read_csv("data/final_table_dist-export.csv") #this should reflect the path where you downlad the dataset
-all_export=pd.read_csv("data/final_table_dist-import.csv") #this should reflect the path where you downlad the dataset
+all_export=pd.read_csv("data/final_table_dist-import.csv") 
 
 
-imp_boat_type=all_import.groupby(['port','type'])['boat'].sum().unstack() #we use the groupby function of pandas dateframe to count the total number of boat for both type (vessel vs steamboat) for each port
+imp_boat_type=all_import.groupby(['port','type'])['boat'].sum().unstack() #we use the groupby function of pandas dateframe to count the total number of boat for both type (vessel vs steamboat) for each port for all the year of the dataset (below I started a loop to do the same year by year)
 exp_boat_type=all_export.groupby(['port','type'])['boat'].sum().unstack() #same for export
 
 
-###plot the graph:
+###plot the graph "number of vessel vs number of steamboat"
 
-pl.subplot(121) #subrgraph for export
+pl.subplot(121) #subgraph for export
 pl.plot(range(1,1000000),range(1,1000000))  #the x=y line to see if ratio > or < 1 
 pl.scatter(exp_boat_type['vessel'],exp_boat_type['steamboat'])  #plot the port given number of vessel and number of steamboat
 
-#we add label to the plot !!!Problem here: in the data set there are spaces in the port names for aestetical reason, python doesn't like it
+#we add label to the plot !!!WARNING Problem here: in the dataset there are spaces in the port names for aestetical reason, python doesn't like it so this loop doesn't work
 for label in exp_boat_type.index:
     pl.annotate(label,(exp_boat_type['vessel'][label],exp_boat_type['steamboat'][label]))  #add the name of the port on the plot
-#exp_boat_type.index allow to get the name of the port
 
 #set axes in log scale
 pl.semilogx()
@@ -31,22 +30,18 @@ pl.semilogy()
 pl.title("export")
 
 
-###We do all the same to plot the graph for the import:
+###We do all the same to generate a subgraph for the import:
 pl.subplot(122)
 pl.plot(range(1,1000000),range(1,1000000)) 
-pl.scatter(imp_boat_type['vessel'],imp_boat_type['steamboat'])  #plot the port given number of vessel and number of steamboat
+pl.scatter(imp_boat_type['vessel'],imp_boat_type['steamboat']) 
 for label in imp_boat_type.index:
-    pl.annotate(label,(exp_boat_type['vessel'][label],exp_boat_type['steamboat'][label]))  #add the name of the port on the plot
-
+    pl.annotate(label,(exp_boat_type['vessel'][label],exp_boat_type['steamboat'][label]))  
 pl.semilogx()
 pl.semilogy() 
-
-
-
 pl.title("import")
 
 
-pl.show()
+pl.show() 
 
 ###it sould be easy to look at how this is evolving through by looking at the data for every year wit something like that:
 
